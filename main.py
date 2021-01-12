fd98d117243abe4d2728be09833ddb6a296d8a9f
bamcasa <combeesang@gmail.com>
2021-01-13 00:03:49 +0900

    inital commit

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
-
 import random
+import math
 
-n=1000
+n=2000
 count=0
 data = []
 data2 = []
+
 for i in range(n):
   x1=random.uniform(-1,1)
   y1=random.uniform(-1,1)
   if (x1 * x1 + y1 * y1 < 1):
-      data2.append([x1,y1])
-  data.append([x1,y1])
+    data2.append([x1,y1])
+  else:
+    data.append([x1,y1])
+
+plt.figure(figsize = (8,8))
 
 x, y = zip(*data)
 plt.scatter(x, y)
 
 x, y = zip(*data2)
 plt.scatter(x, y)
-plt.figure(figsize=(12, 12))
 
 plt.show()
+
+Pi = 4 * len(data2)/n
+print(f"Pi : {Pi}")
+print(f"Difference: {round(abs(math.pi - Pi),4)}")
